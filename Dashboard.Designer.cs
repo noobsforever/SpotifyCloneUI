9fb5ac012fe5d92b8b5f5365d88a2a8a34a4bcb8
SunderAli416 <sunderlaghari416@gmail.com>
2020-12-19 12:16:26 +0500

    Home Page bre dark theme

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -36,7 +36,14 @@ namespace SpotifyCloneUI
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.playlistEntry1 = new SpotifyCloneUI.PlaylistEntry();
+            this.playlistEntry2 = new SpotifyCloneUI.PlaylistEntry();
+            this.playlistEntry3 = new SpotifyCloneUI.PlaylistEntry();
+            this.playlistEntry4 = new SpotifyCloneUI.PlaylistEntry();
+            this.playlistEntry5 = new SpotifyCloneUI.PlaylistEntry();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // homeButton
@@ -128,7 +135,7 @@ namespace SpotifyCloneUI
             this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
             this.guna2TextBox1.IconLeft = ((System.Drawing.Image)(resources.GetObject("guna2TextBox1.IconLeft")));
-            this.guna2TextBox1.Location = new System.Drawing.Point(414, 59);
+            this.guna2TextBox1.Location = new System.Drawing.Point(493, 59);
             this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2TextBox1.Name = "guna2TextBox1";
             this.guna2TextBox1.PasswordChar = '\0';
@@ -139,11 +146,60 @@ namespace SpotifyCloneUI
             this.guna2TextBox1.Size = new System.Drawing.Size(324, 43);
             this.guna2TextBox1.TabIndex = 3;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.playlistEntry1);
+            this.flowLayoutPanel1.Controls.Add(this.playlistEntry2);
+            this.flowLayoutPanel1.Controls.Add(this.playlistEntry3);
+            this.flowLayoutPanel1.Controls.Add(this.playlistEntry4);
+            this.flowLayoutPanel1.Controls.Add(this.playlistEntry5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(265, 125);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(769, 468);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // playlistEntry1
+            // 
+            this.playlistEntry1.Location = new System.Drawing.Point(3, 3);
+            this.playlistEntry1.Name = "playlistEntry1";
+            this.playlistEntry1.Size = new System.Drawing.Size(739, 84);
+            this.playlistEntry1.TabIndex = 0;
+            // 
+            // playlistEntry2
+            // 
+            this.playlistEntry2.Location = new System.Drawing.Point(3, 93);
+            this.playlistEntry2.Name = "playlistEntry2";
+            this.playlistEntry2.Size = new System.Drawing.Size(739, 84);
+            this.playlistEntry2.TabIndex = 1;
+            // 
+            // playlistEntry3
+            // 
+            this.playlistEntry3.Location = new System.Drawing.Point(3, 183);
+            this.playlistEntry3.Name = "playlistEntry3";
+            this.playlistEntry3.Size = new System.Drawing.Size(739, 84);
+            this.playlistEntry3.TabIndex = 2;
+            // 
+            // playlistEntry4
+            // 
+            this.playlistEntry4.Location = new System.Drawing.Point(3, 273);
+            this.playlistEntry4.Name = "playlistEntry4";
+            this.playlistEntry4.Size = new System.Drawing.Size(739, 84);
+            this.playlistEntry4.TabIndex = 3;
+            // 
+            // playlistEntry5
+            // 
+            this.playlistEntry5.Location = new System.Drawing.Point(3, 363);
+            this.playlistEntry5.Name = "playlistEntry5";
+            this.playlistEntry5.Size = new System.Drawing.Size(739, 84);
+            this.playlistEntry5.TabIndex = 4;
+            // 
             // Dashboard
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(212)))));
             this.ClientSize = new System.Drawing.Size(1073, 605);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.guna2TextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.usernameDisplay);
@@ -156,6 +212,7 @@ namespace SpotifyCloneUI
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Dashboard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,5 +225,11 @@ namespace SpotifyCloneUI
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private PlaylistEntry playlistEntry1;
+        private PlaylistEntry playlistEntry2;
+        private PlaylistEntry playlistEntry3;
+        private PlaylistEntry playlistEntry4;
+        private PlaylistEntry playlistEntry5;
     }
 }
---
+++ PlaylistEntry.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace SpotifyCloneUI
+{
+    partial class PlaylistEntry
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlaylistEntry));
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 25;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
+            this.guna2Button1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2Button1.ImageSize = new System.Drawing.Size(60, 60);
+            this.guna2Button1.Location = new System.Drawing.Point(0, 3);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(736, 77);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "Playlist Name";
+            // 
+            // PlaylistEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2Button1);
+            this.Name = "PlaylistEntry";
+            this.Size = new System.Drawing.Size(739, 84);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
